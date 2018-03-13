d2f8b8090fc8979305ad878257e71b6d5d9b6d32
weifeng.ge <lg31415@163.com>
2018-03-13 22:13:51 +0800

    cocoweb.py

--- www/orm.py
+++ www/orm.py
@@ -83,9 +83,9 @@ class Field(object):
         return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
     #一下是几种具体的列名的数据类型
 class StringField(Field):
-    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100')
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
         super().__init__(name,ddl,primary_key,default)
-class BoonleanField(Field):
+class BooleanField(Field):
     def __init__(self,name=None,default=False):
         super().__init__(name,'boolean',False,default)
 class IntegerField(Field):
@@ -164,10 +164,10 @@ class Model(dict,metaclass=ModelMetaclass):
                  logging.debug('ussing  default value for %s :%s' % (key,str(value)))
                  setattr(self,key,value)
          return value
-     #新的语法 @classmethod 装时期用于把类里面定义的方法声明为该类的类方法
-     #获取表里面符合条件的所有数据，类方法的第一个参数为该类名
-    @classmethod
-    async def findAll(cls,where=None,args=None,**kw):
+    #新的语法 @classmethod 装时期用于把类里面定义的方法声明为该类的类方法
+    #获取表里面符合条件的所有数据，类方法的第一个参数为该类名
+     @classmethod
+     async def findAll(cls,where=None,args=None,**kw):
         'find objects by where clause.'
         sql=[cls.__select__]
         if where:
@@ -193,8 +193,8 @@ class Model(dict,metaclass=ModelMetaclass):
             rs=await select(''.join(sql),args)
             return [cls(**r) for r in rs]
     # 该方法不了解
-    @classmethod
-    async def findNumber(cls,selectField,where=None,args=None):
+     @classmethod
+     async def findNumber(cls,selectField,where=None,args=None):
         'find number by select and where .'
         sql=['select %s _num_ from `%s`'% (selectField,cls.__table__)]
         if where:
@@ -205,8 +205,8 @@ class Model(dict,metaclass=ModelMetaclass):
             return None
         return rs[0]['_num_']
     #主键查找方法
-    @classmethod
-    async def find(cls,pk):
+     @classmethod
+     async def find(cls,pk):
         ' find object by primary key. '
         rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
         if len(rs) == 0:
@@ -214,21 +214,21 @@ class Model(dict,metaclass=ModelMetaclass):
         return cls(**rs[0])
     #一下的都是对象方法,所以可以不用传任何参数,方法内部可以使用该对象的所有属性,及其方便
     #保存实例到数据库
-    async def save(self):
+     async def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primary_key__))
         rows = await execute(self.__insert__, args)
         if rows != 1:
             logging.warn('failed to insert record: affected rows: %s' % rows)
     #更新数据库数据
-    async def update(self):
+     async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primary_key__))
         rows = await execute(self.__update__, args)
         if rows != 1:
             logging.warn('failed to update by primary key: affected rows: %s' % rows)
     #删除数据
-    async def remove(self):
+     async def remove(self):
         args = [self.getValue(self.__primary_key__)]
         rows = await execute(self.__delete__, args)
         if rows != 1:
